58f45f78f6fdd41eeb0bed73e6df20895d959d39
유도영 <dbehdud35@naver.com>
2024-06-05 00:06:33 +0900

    feat: meals page and loading page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,20 +1,51 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
 
-const Meals = () => {
+/**
+ * 데이터를 관리하는 부분을 밖으로 아웃소싱 했다는 장점
+ */
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+const MealsPage = () => {
+  /**
+   * Server component이기 때문에 async 함수로 작성이 가능!!
+   */
+  const meals = getMeals();
+
+  /**
+   * Suspense는 React에서 제공하는 컴포넌트
+   * 일부 데이터 또는 리소스가 불러와질 때까지 로딩 상태를 처리하고 대체 컨텐츠를 표시
+   */
   return (
-    <div>
-      <h1>This is Meals Page!!</h1>
-      <p>
-        <Link href="/meals/share">Share</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal1">Meal1</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal2">Meal2</Link>
-      </p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite meal and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 };
 
-export default Meals;
+export default MealsPage;
